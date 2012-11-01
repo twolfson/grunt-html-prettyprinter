f33dd5ddb17d73a0335b1c8d56912734689f38f7
Todd Wolfson <todd@twolfson.com>
2012-10-31 20:58:26 -0700

    Copied over task

--- tasks/html-prettyprinter.js
+++ tasks/html-prettyprinter.js
@@ -5,8 +5,8 @@
  * Copyright (c) 2012 Todd Wolfson
  * Licensed under the MIT license.
  */
-
-module.exports = function(grunt) {
+var htmlPrettyprinter = require('html').prettyPrint;
+module.exports = function (grunt) {
 
   // Please see the grunt documentation for more information regarding task and
   // helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
@@ -15,16 +15,29 @@ module.exports = function(grunt) {
   // TASKS
   // ==========================================================================
 
-  grunt.registerTask('html-prettyprinter', 'Your task description goes here.', function() {
-    grunt.log.write(grunt.helper('html-prettyprinter'));
+  grunt.registerMultiTask('html-prettyprinter', 'Prettyprint HTML from src to dest', function () {
+    // Collect the filepaths we need
+    var file = this.file,
+        src = file.src,
+        srcFiles = grunt.file.expand(src),
+        dest = file.dest;
+
+    // Read in the srcFiles, join, and beautify it
+    var srcContent = srcFiles.map(grunt.file.read),
+        srcBlob = srcContent.join('\n'),
+        beautifiedContent = grunt.helper('html-prettyprinter', srcBlob);
+
+    // Write out the content
+    grunt.file.write(dest, beautifiedContent);
   });
 
   // ==========================================================================
   // HELPERS
   // ==========================================================================
 
-  grunt.registerHelper('html-prettyprinter', function() {
-    return 'html-prettyprinter!!!';
+  grunt.registerHelper('html-prettyprinter', function (content) {
+    var beautifiedContent = htmlPrettyprinter(content);
+    return beautifiedContent;
   });
 
 };
